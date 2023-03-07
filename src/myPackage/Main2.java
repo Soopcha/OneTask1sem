929453c83b505931108140fef01fee04a5a2450e
Soopcha <lena.malykhina.04@gmail.com>
2023-03-07 17:34:53 +0300

    2

--- src/myPackage/Main2.java
+++ src/myPackage/Main2.java
@@ -3,6 +3,8 @@ package myPackage;
 public class Main2 {
     public static void main(String[] args) {
         BasedNumber2 num1 = new BasedNumber2("9DDDC",16);  // в 16 ричной следует писать строкой
+        BasedNumber2 num2 = new BasedNumber2(4567,16);
         System.out.println(num1.convert(8).getNumber());
+        System.out.println(num2.convert(8).getNumber());
     }
 }
